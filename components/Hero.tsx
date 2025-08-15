ff41d3163883b00b8cec86a8a790b72d28df938d
Andres Neira <114157683+AndresNei20@users.noreply.github.com>
2025-08-14 22:44:32 -0500

    hero changed

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,8 +1,9 @@
-import { FaLocationArrow } from "react-icons/fa6";
+import { FaFileLines, FaLinkedin, FaLocationArrow } from "react-icons/fa6";
 
 import MagicButton from "./MagicButton";
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import Link from "next/link";
 
 const Hero = () => {
   return (
@@ -20,7 +21,13 @@ const Hero = () => {
           className="h-[80vh] w-[50vw] top-10 left-full"
           fill="purple"
         />
+        <Spotlight
+          className="hidden md:block h-[120vh] w-[50vw] top-72 left-full"
+          fill="orange"
+        />
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+
+        <Spotlight className="left-96 top-28 h-[80vh] w-[50vw]" fill="orange" />
       </div>
 
       {/**
@@ -43,7 +50,7 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Is not just about development, it&apos;s about user experience
           </p>
 
           {/**
@@ -52,24 +59,36 @@ const Hero = () => {
            *  change md:text-6xl, add more responsive code
            */}
           <TextGenerateEffect
-            words="Transforming Concepts into Seamless User Experiences"
+            words="Bringing your Concepts into Seamless User Experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
           <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
-            Hi! I&apos;m Adrian, a Next.js Developer based in Croatia.
+            Hi! I&apos;m Andres Neira, a Frontend Developer based in Colombia.
           </p>
 
-          <a href="#about">
-            <MagicButton
-              title="Show my work"
-              icon={<FaLocationArrow />}
-              position="right"
-            />
-          </a>
+          <div className="flex flex-col mt-10 md:flex-row gap-4">
+
+            <a href="https://www.linkedin.com/in/andresnei">
+              <MagicButton
+                title="LinkedIn"
+                icon={<FaLinkedin />}
+                position="right"
+              />
+            </a>
+
+            <a href="https://www.linkedin.com/in/andresnei">
+              <MagicButton
+                title="CV"
+                icon={<FaFileLines />}
+                position="right"
+              />
+            </a>
+          </div>
+
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
